e1c804e5fd0b3ed8488e3242977c25c7c98b34b8
Ricky Abraham <Ricky.Abraham@einfochips.com>
2023-03-02 12:28:21 +0530

    added pytest fixture

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,13 +1,26 @@
+import pytest
 from selenium import webdriver
 from assertpy import assert_that
+from selenium.webdriver.common.by import By
+
 
 class TestLoginUI:
 
-    def test_title(self):
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-        driver.implicitly_wait(20)
-        driver.get("https://opensource-demo.orangehrmlive.com/")
-        actual_title = driver.title
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(20)
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        yield
+        self.driver.quit()
+
+    def test_title(self, setup):
+        actual_title = self.driver.title
         # assert actual_title == "OrangeHRM"
         assert_that("OrangeHRM").is_equal_to(actual_title)
+
+    def test_header(self, setup):
+        text = self.driver.find_element(By.XPATH,"//h5[text()='Login']").text
+        assert_that("Login").is_equal_to(text)
+
