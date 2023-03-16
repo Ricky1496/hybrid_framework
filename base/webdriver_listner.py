8c7a7d7e046f6dac156024315c8797496f4b3cf1
Ricky Abraham <Ricky.Abraham@einfochips.com>
2023-03-16 11:18:23 +0530

    Updated new practice codes

--- base/webdriver_listner.py
+++ base/webdriver_listner.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from utilities import read_utils
+
+"""Browser Configuration"""
 
 
 class WebDriverWrapper:
@@ -8,8 +10,15 @@ class WebDriverWrapper:
 
     @pytest.fixture(scope="function", autouse=True)
     def browser_config(self):
-        serv_driver = Service(executable_path=r"C:\Users\JiDi\Downloads\chromedriver_win32 (5)\chromedriver.exe")
-        self.driver = webdriver.Chrome(service=serv_driver)
+        browser_name = read_utils.get_value_from_json("../test_data/data.json", "browser")
+
+        if browser_name == "edge":
+            self.driver = webdriver.Edge()
+        elif browser_name == "ff":
+            self.driver = webdriver.Firefox()
+        else:
+            self.driver = webdriver.Chrome()
+
         self.driver.maximize_window()
         self.driver.implicitly_wait(20)
         self.driver.get("https://opensource-demo.orangehrmlive.com/")
